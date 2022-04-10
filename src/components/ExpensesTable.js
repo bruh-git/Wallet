f6976790c45f7261f6f7a99450b22c285e4a8dd8
Bruna <brunac.cardososilva@gmail.com>
2022-04-10 13:46:21 -0300

    Requisito 8
    Em desenvolvimento
    
    Concluido.

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -4,21 +4,45 @@ import { connect } from 'react-redux';
 
 class ExpensesTable extends Component {
   render() {
-  // const { expenses } = this.props;
+    const { expenses } = this.props;
     return (
-      <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-      </table>
+      <div>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
+        {expenses.map((el) => (
+          <tr key={ el.id }>
+            <td>{ el.description }</td>
+            <td>{ el.tag }</td>
+            <td>{ el.method }</td>
+            <td>{ Number(el.value).toFixed(2) }</td>
+            <td>{ el.exchangeRates[el.currency].name }</td>
+            <td>{ Number(el.exchangeRates[el.currency].ask).toFixed(2) }</td>
+            <td>
+              {
+                (parseFloat(el.value)
+                  * parseFloat(el.exchangeRates[el.currency].ask))
+                  .toFixed(2)
+              }
+            </td>
+            <td>
+              <td>Real</td>
+              <button type="submit">Editar</button>
+              <button type="submit">Excluir</button>
+            </td>
+          </tr>
+        ))}
+      </div>
     );
   }
 }
